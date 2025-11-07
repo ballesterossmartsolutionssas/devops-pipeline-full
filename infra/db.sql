1ce3a111c6e7364f1a2c1d30448e1a30364460ad
ballesterossmartsolutionssas <ballesterossmartsolutionssas@gmail.com>
2025-11-07 15:59:11 -0500

    chore: agregar script de base de datos para tabla users

--- infra/db.sql
+++ infra/db.sql
@@ -1,6 +1,5 @@
--- infra/db.sql
 -- Create a table to store users
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  id SERIAL PRIMARY KEY, -- Auto-incrementing ID
+  name VARCHAR(100) NOT NULL -- User name, required
 );
